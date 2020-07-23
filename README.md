888c8213851ed664c212e3b77d9ea2b874371ffb
Hilton Girardi Presa <hiltongp@gmail.com>
2020-07-23 14:47:55 -0300

    Versão inicial
    Teste ok, funcional

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-project-starter-eclipse-jsf-primefaces-jpa-wildfly
+Starter Project for Eclipse
+	JDK 11
+	Wildfly 19.1.00
+	JSF
+	Primefaces 8.0
+	JPA (H2)
+	JakartaEE 8.0.0
+	
+	
+Eclipse IDE for Enterprise Java Developers (includes Incubating components)
+
+Version: 2020-06 (4.16.0)
+Build id: 20200615-1200
---
+++ project/src/main/java/com/example/project/view/Bean.java
@@ -0,0 +1,41 @@
+package com.example.project.view;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.example.project.model.Message;
+import com.example.project.service.MessageService;
+
+@Named 
+@RequestScoped
+public class Bean {
+
+    private Message message = new Message();
+    private List<Message> messages;
+
+    @Inject
+    private MessageService messageService;
+
+    @PostConstruct
+    public void init() {
+        messages = messageService.list();
+    }
+    
+    public void submit() {
+        messageService.create(message);
+        messages.add(message);
+        message = new Message();
+    }
+    
+    public Message getMessage() {
+        return message;
+    }
+    
+    public List<Message> getMessages() {
+        return messages;
+    }
+}
